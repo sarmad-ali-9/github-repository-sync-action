69b710a0fdd284d5b8912f0dc2876571abd544e0
Glend Maatita <glend.maatita@gmail.com>
2023-01-16 09:21:12 +0700

    adjust config

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,7 @@ mkdir -p /root/.ssh
 echo "$INPUT_SSH_PRIVATE_KEY" > /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
 
+git config --global --add safe.directory /github/workspace
 git remote add destination "$INPUT_TARGET_REPO_URL"
-git push destination "$GITHUB_REF:$GITHUB_REF" -f
+git fetch --unshallow
+git push destination "$INPUT_SOURCE_BRANCH:$INPUT_DESTINATION_BRANCH" -f
